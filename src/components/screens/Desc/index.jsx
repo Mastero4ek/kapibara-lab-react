000d0ff7f0c2e61186caf7c9c0deb8bbcdb20b26
Ch-P. Viacheslav <165379225+Mastero4ekPortfolio@users.noreply.github.com>
2024-04-10 14:43:55 +0300

    add Typewriter effect

--- src/components/screens/Desc/index.jsx
+++ src/components/screens/Desc/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 import DescStyle from './Desc.module.scss';
 
 export const Desc = () => {
+    const descString = `Добро пожаловать в медицинскую лабораторию KapibaraLab, где работают исключительно капибары — ваш
+    надежный партнер в обеспечении качественной и точной диагностики!`;
+
     return (
         <div className="container">
-            <p className={DescStyle.root}>
-                Добро пожаловать в медицинскую лабораторию KapibaraLab, где работают исключительно капибары — ваш
-                надежный партнер в обеспечении качественной и точной диагностики! </p>
+            <div className={DescStyle.root}>
+                <Typewriter
+                    options={{ cursor: "", delay: 30 }}
+                    onInit={(typewriter) => { typewriter.typeString(descString).start(); }}
+                />
+            </div>
         </div>
     )
 }
